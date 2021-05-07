6570af1989c46d5d971d8857d424a162c1f1c517
siteshmehta <mehtasitesh@gmail.com>
2021-05-08 01:31:35 +0530

    Added txn type dropdown in amount

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -3,7 +3,8 @@ import { GlobalContext } from "../context/GlobalState";
 
 export default function AddTransaction() {
   const [text, setText] = useState("");
-  const [amount, setamount] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const [transactionType, setTransactionType] = useState("");
   const { addTransaction } = useContext(GlobalContext);
 
   const onSubmit = (e) => {
@@ -12,15 +13,20 @@ export default function AddTransaction() {
       alert("Please enter the value");
       return;
     }
+    if (!transactionType) {
+      alert("Select transaction type");
+      return;
+    }
     const newTransaction = {
       id: Math.floor(Math.random() * 10000),
       text,
-      amount: +amount,
+      amount: transactionType === "-" ? Number(-amount) : Number(amount),
     };
 
     addTransaction(newTransaction);
     setText("");
-    setamount(0);
+    setAmount(0);
+    setTransactionType("");
   };
 
   return (
@@ -31,7 +37,7 @@ export default function AddTransaction() {
 
       <div className="col-12  px-0">
         <form onSubmit={onSubmit}>
-          <p class="m-0 p-0">Text</p>
+          <p className="m-0 p-0">Text</p>
           <input
             type="text"
             className="form-control shadow-none"
@@ -39,18 +45,30 @@ export default function AddTransaction() {
             onChange={(e) => setText(e.target.value)}
             placeholder={"Enter the transaction name"}
           />
-          <p class=" m-0 mt-2 p-0">
-            Amount &nbsp;
-            <span className="text-secondary m-0 p-0">
-              (Negative- expenses, positive - income )
-            </span>
-          </p>
-          <input
-            type="number"
-            className="form-control shadow-none"
-            value={amount}
-            onChange={(e) => setamount(e.target.value)}
-          />
+          <p className=" m-0 mt-2 p-0">Amount &nbsp;</p>
+          <div className="input-group input-group-sm">
+            <div class="input-group-text">
+              <select
+                className=" form-control-sm"
+                value={transactionType}
+                onChange={(e) => setTransactionType(e.target.value)}
+              >
+                <option disabled={true} value="">
+                  Select txn. type
+                </option>
+                <option value="+">Income</option>
+                <option value="-">Expense</option>
+              </select>
+            </div>
+            <input
+              type="number"
+              className="form-control shadow-none"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              min="0"
+              step="1"
+            />
+          </div>
           <div className="d-grid mt-4">
             <button
               type="submit"
